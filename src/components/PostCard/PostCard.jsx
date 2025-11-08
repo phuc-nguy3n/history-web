af88347efe50ef9850152ff8db4240c346c34509
nguyenphucvt112 <nguyenphucvt112@gmail.com>
2025-11-08 10:35:55 +0000

    add price & hours open

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function PostCard({ id, thumbnail, date, title, excerpt }) {
+export default function PostCard({
+  id,
+  thumbnail,
+  date,
+  title,
+  excerpt,
+  openingTime,
+  closingTime,
+  ticketPrice,
+}) {
   return (
     <article className="group overflow-hidden rounded-lg border border-slate-200 bg-white shadow-sm transition-shadow hover:shadow-md">
       <Link to={`/posts/${id}`} className="block">
@@ -33,6 +42,44 @@ export default function PostCard({ id, thumbnail, date, title, excerpt }) {
               {excerpt}
             </p>
           )}
+          <div className="mt-4 flex justify-between text-xs text-slate-500">
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-1 h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>
+                {openingTime || "7:00 AM"} - {closingTime || "5:00 PM"}
+              </span>
+            </div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-1 h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
+                />
+              </svg>
+              <span>{ticketPrice || "100.000đ"}</span>
+            </div>
+          </div>
         </div>
       </Link>
     </article>
